53301f1fda08bd69530c81639eade5ce9c779d00
Xiaojun Jia <jxj2297903127@gmail.com>
2024-11-11 23:18:48 -0600

    Import sokoban_solver content

---
+++ envs/solvers/sokoban_solver/sokoban.py
@@ -0,0 +1,234 @@
+import numpy as np
+import heapq
+
+class PriorityQueue:
+    """Define a PriorityQueue data structure that will be used"""
+    def __init__(self):
+        self.Heap = []
+        self.Count = 0
+
+    def push(self, item, priority):
+        entry = (priority, self.Count, item)
+        heapq.heappush(self.Heap, entry)
+        self.Count += 1
+
+    def pop(self):
+        (_, _, item) = heapq.heappop(self.Heap)
+        return item
+
+    def isEmpty(self):
+        return len(self.Heap) == 0
+
+def transferToGameState(layout):
+    """Transfer the layout of initial puzzle"""
+    layout = [x.replace('\n','') for x in layout]
+    layout = [','.join(layout[i]) for i in range(len(layout))]
+    layout = [x.split(',') for x in layout]
+    maxColsNum = max([len(x) for x in layout])
+    for irow in range(len(layout)):
+        for icol in range(len(layout[irow])):
+            if layout[irow][icol] == ' ': layout[irow][icol] = 0   # free space
+            elif layout[irow][icol] == '#': layout[irow][icol] = 1 # wall
+            elif layout[irow][icol] == '&': layout[irow][icol] = 2 # player
+            elif layout[irow][icol] == 'B': layout[irow][icol] = 3 # box
+            elif layout[irow][icol] == '.': layout[irow][icol] = 4 # goal
+            elif layout[irow][icol] == 'X': layout[irow][icol] = 5 # box on goal
+        colsNum = len(layout[irow])
+        if colsNum < maxColsNum:
+            layout[irow].extend([1 for _ in range(maxColsNum-colsNum)]) 
+    return np.array(layout)
+
+def PosOfPlayer(gameState):
+    """Return the position of agent"""
+    return tuple(np.argwhere(gameState == 2)[0])
+
+def PosOfBoxes(gameState):
+    """Return the positions of boxes"""
+    return tuple(tuple(x) for x in np.argwhere((gameState == 3) | (gameState == 5)))
+
+def PosOfWalls(gameState):
+    """Return the positions of walls"""
+    return tuple(tuple(x) for x in np.argwhere(gameState == 1))
+
+def PosOfGoals(gameState):
+    """Return the positions of goals"""
+    return tuple(tuple(x) for x in np.argwhere((gameState == 4) | (gameState == 5)))
+
+def isEndState(posBox):
+    """Check if all boxes are on the goals (i.e. pass the game)"""
+    return sorted(posBox) == sorted(posGoals)
+
+def isLegalAction(action, posPlayer, posBox):
+    """Check if the given action is legal"""
+    xPlayer, yPlayer = posPlayer
+    if action[-1].isupper(): # the move was a push
+        x1, y1 = xPlayer + 2 * action[0], yPlayer + 2 * action[1]
+    else:
+        x1, y1 = xPlayer + action[0], yPlayer + action[1]
+    return (x1, y1) not in posBox + posWalls
+
+def legalActions(posPlayer, posBox):
+    """Return all legal actions for the agent in the current game state"""
+    allActions = [[-1,0,'u','U'],[1,0,'d','D'],[0,-1,'l','L'],[0,1,'r','R']]
+    xPlayer, yPlayer = posPlayer
+    legalActions = []
+    for action in allActions:
+        x1, y1 = xPlayer + action[0], yPlayer + action[1]
+        if (x1, y1) in posBox: # the move was a push
+            action.pop(2) # drop the little letter
+        else:
+            action.pop(3) # drop the upper letter
+        if isLegalAction(action, posPlayer, posBox):
+            legalActions.append(action)
+        else: 
+            continue     
+    return tuple(tuple(x) for x in legalActions)
+
+def updateState(posPlayer, posBox, action):
+    """Return updated game state after an action is taken"""
+    xPlayer, yPlayer = posPlayer
+    newPosPlayer = [xPlayer + action[0], yPlayer + action[1]]
+    posBox = [list(x) for x in posBox]
+    if action[-1].isupper():
+        posBox.remove(newPosPlayer)
+        posBox.append([xPlayer + 2 * action[0], yPlayer + 2 * action[1]])
+    posBox = tuple(tuple(x) for x in posBox)
+    newPosPlayer = tuple(newPosPlayer)
+    return newPosPlayer, posBox
+
+def isFailed(posBox):
+    """This function used to observe if the state is potentially failed, then prune the search"""
+    rotatePattern = [[0,1,2,3,4,5,6,7,8],
+                    [2,5,8,1,4,7,0,3,6],
+                    [0,1,2,3,4,5,6,7,8][::-1],
+                    [2,5,8,1,4,7,0,3,6][::-1]]
+    flipPattern = [[2,1,0,5,4,3,8,7,6],
+                    [0,3,6,1,4,7,2,5,8],
+                    [2,1,0,5,4,3,8,7,6][::-1],
+                    [0,3,6,1,4,7,2,5,8][::-1]]
+    allPattern = rotatePattern + flipPattern
+
+    for box in posBox:
+        if box not in posGoals:
+            board = [(box[0] - 1, box[1] - 1), (box[0] - 1, box[1]), (box[0] - 1, box[1] + 1), 
+                    (box[0], box[1] - 1), (box[0], box[1]), (box[0], box[1] + 1), 
+                    (box[0] + 1, box[1] - 1), (box[0] + 1, box[1]), (box[0] + 1, box[1] + 1)]
+            for pattern in allPattern:
+                newBoard = [board[i] for i in pattern]
+                if newBoard[1] in posWalls and newBoard[5] in posWalls: return True
+                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posWalls: return True
+                elif newBoard[1] in posBox and newBoard[2] in posWalls and newBoard[5] in posBox: return True
+                elif newBoard[1] in posBox and newBoard[2] in posBox and newBoard[5] in posBox: return True
+                elif newBoard[1] in posBox and newBoard[6] in posBox and newBoard[2] in posWalls and newBoard[3] in posWalls and newBoard[8] in posWalls: return True
+    return False
+
+def heuristic(posPlayer, posBox):
+    """A heuristic function to calculate the overall distance between the boxes and goals"""
+    distance = 0
+    completes = set(posGoals) & set(posBox)
+    sortposBox = list(set(posBox).difference(completes))
+    sortposGoals = list(set(posGoals).difference(completes))
+    for i in range(len(sortposBox)):
+        distance += (abs(sortposBox[i][0] - sortposGoals[i][0])) + (abs(sortposBox[i][1] - sortposGoals[i][1]))
+    return distance
+
+def cost(actions):
+    """A cost function"""
+    return len([x for x in actions if x.islower()])
+
+def aStarSearch():
+    """Implement aStarSearch approach"""
+    beginBox = PosOfBoxes(gameState)
+    beginPlayer = PosOfPlayer(gameState)
+
+    start_state = (beginPlayer, beginBox)
+    frontier = PriorityQueue()
+    try:
+        frontier.push([start_state], heuristic(beginPlayer, beginBox))
+    except:
+        return None
+    exploredSet = set()
+    actions = PriorityQueue()
+    actions.push([0], heuristic(beginPlayer, start_state[1]))
+    while frontier:
+        node = frontier.pop()
+        node_action = actions.pop()
+        if isEndState(node[-1][-1]):
+            # print(','.join(node_action[1:]).replace(',',''))
+            break
+        if node[-1] not in exploredSet:
+            exploredSet.add(node[-1])
+            Cost = cost(node_action[1:])
+            for action in legalActions(node[-1][0], node[-1][1]):
+                newPosPlayer, newPosBox = updateState(node[-1][0], node[-1][1], action)
+                if isFailed(newPosBox):
+                    continue
+                Heuristic = heuristic(newPosPlayer, newPosBox)
+                frontier.push(node + [(newPosPlayer, newPosBox)], Heuristic + Cost) 
+                actions.push(node_action + [action[-1]], Heuristic + Cost)
+    return node_action[1:]
+
+
+def save_sokoban_map_to_text(env, filename="solvers/sokoban_solver/sokobanLevels/sokoban_map.txt"):
+    """
+    Saves the current Sokoban map layout to a text file with specific symbols.
+    
+    Symbols:
+    - '#' for walls
+    - '.' for destination
+    - 'B' for box
+    - 'X' for box on destination
+    - '&' for player
+    - '%' for player on destination
+    - ' ' for empty/movable space
+    """
+    # Map numeric values to specified symbols
+    symbol_map = {
+        0: '#',  # Wall
+        1: ' ',  # Movable space
+        2: '.',  # Destination
+        3: 'X',  # box on target
+        4: 'B',  # box not on target
+        5: '&',  # player
+    }
+    
+    # Get the current state of the map
+    env_state = env.render(mode='rgb_array')
+    if hasattr(env, 'unwrapped'):
+        env_state = env.unwrapped.room_state  # Accessing the internal map grid if exposed
+
+    map_text_output = []
+    for row in env_state:
+        map_text = ""
+        for cell in row:
+            map_text += symbol_map.get(cell, ' ')  # Default to ' ' if unknown value
+        map_text += '\n'
+        map_text_output.append(map_text)
+    return map_text_output
+    
+
+def solve_sokoban(env):
+    layout = save_sokoban_map_to_text(env)
+    global gameState, posWalls, posGoals
+
+    gameState = transferToGameState(layout)
+    posWalls = PosOfWalls(gameState)
+    posGoals = PosOfGoals(gameState)
+    actions = aStarSearch()
+
+    if actions is None:
+        return None
+
+    # convert action into environment format
+    action_dict = {'u': 0,
+                   'U': 0,
+                   'd': 1,
+                   'D': 1,
+                   'l': 2,
+                   'L': 2,
+                   'r': 3,
+                   'R': 3}
+    output = []
+    for elem in actions:
+        output.append(action_dict[elem])
+    return output
